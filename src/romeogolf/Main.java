b03ddc3c7f81f18a432367e3ff961558de08a9b0
RomeoGolf <romeogolf@mail.ru>
2013-10-24 10:51:14 +0600

    Ввел компоненты ввода цифр - 4 поля с кнопками увеличения и уменьшения. Заготовка.

--- src/romeogolf/Main.java
+++ src/romeogolf/Main.java
@@ -17,11 +17,23 @@ import javafx.scene.paint.*;
 import javafx.geometry.*;
 
 public class Main extends Application {
-	public TextField userTextField;
+	public TextField tfDigit1;
+	public TextField tfDigit2;
+	public TextField tfDigit3;
+	public TextField tfDigit4;
+	public Button btUp1;
+	public Button btUp2;
+	public Button btUp3;
+	public Button btUp4;
+
+	public Button btDown1;
+	public Button btDown2;
+	public Button btDown3;
+	public Button btDown4;
 	EventHandler<ActionEvent> eh = new EventHandler<ActionEvent>() {
 		    @Override
 		    public void handle(ActionEvent e) {
-			userTextField.setText(Integer.toString(Integer.valueOf(userTextField.getText()) + 1));
+			tfDigit1.setText(Integer.toString(Integer.valueOf(tfDigit1.getText()) + 1));
 		    };
 	};
 
@@ -40,22 +52,41 @@ public class Main extends Application {
 		grid.setPadding(new Insets(25, 25, 25, 25));
 		grid.setGridLinesVisible(true);
 
-//		final TextField userTextField = new TextField("1");
-		userTextField = new TextField("1");
-		userTextField.setPrefColumnCount(1);
-		grid.add(userTextField, 0, 0, 1, 2);
-
-		Button btUp1 = new Button();
-		grid.add(btUp1, 1, 0);
-		Button btDown1 = new Button();
-		grid.add(btDown1, 1, 1);
-//		btUp1.setOnAction(new EventHandler<ActionEvent>() {
-//		    @Override
-//		    public void handle(ActionEvent e) {
-//			userTextField.setText(Integer.toString(Integer.valueOf(userTextField.getText()) + 1));
-//		    }
-//		});
+		tfDigit1 = new TextField("1");
+		tfDigit1.setPrefColumnCount(1);
+		grid.add(tfDigit1, 0, 1);
+		btUp1 = new Button();
+		grid.add(btUp1, 0, 0);
+		btDown1 = new Button();
+		grid.add(btDown1, 0, 2);
 		btUp1.setOnAction(eh);
+
+		tfDigit2 = new TextField("1");
+		tfDigit2.setPrefColumnCount(1);
+		grid.add(tfDigit2, 2, 1);
+		btUp2 = new Button();
+		grid.add(btUp2, 2, 0);
+		btDown2 = new Button();
+		grid.add(btDown2, 2, 2);
+		btUp2.setOnAction(eh);
+
+		tfDigit3 = new TextField("1");
+		tfDigit3.setPrefColumnCount(1);
+		grid.add(tfDigit3, 4, 1);
+		btUp3 = new Button();
+		grid.add(btUp3, 4, 0);
+		btDown3 = new Button();
+		grid.add(btDown3, 4, 2);
+		btUp3.setOnAction(eh);
+
+		tfDigit4 = new TextField("1");
+		tfDigit4.setPrefColumnCount(1);
+		grid.add(tfDigit4, 6, 1);
+		btUp4 = new Button();
+		grid.add(btUp4, 6, 0);
+		btDown4 = new Button();
+		grid.add(btDown4, 6, 2);
+		btUp4.setOnAction(eh);
 
 
 		Scene scene = new Scene(grid, 300, 275);
