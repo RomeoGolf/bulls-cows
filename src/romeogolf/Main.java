2f6716b67711c185dc157341e86f3e08e2e67bd3
RomeoGolf <romeogolf@mail.ru>
2013-10-24 13:18:19 +0600

    Формирование полей ввода и кнопок - выравнивание и все такое.

--- src/romeogolf/Main.java
+++ src/romeogolf/Main.java
@@ -21,6 +21,7 @@ public class Main extends Application {
 	public TextField tfDigit2;
 	public TextField tfDigit3;
 	public TextField tfDigit4;
+
 	public Button btUp1;
 	public Button btUp2;
 	public Button btUp3;
@@ -47,46 +48,75 @@ public class Main extends Application {
 		GridPane grid = new GridPane();
 
 		grid.setAlignment(Pos.CENTER);
-//		grid.setHgap(10);
+		grid.setHgap(5);
 //		grid.setVgap(10);
 		grid.setPadding(new Insets(25, 25, 25, 25));
-		grid.setGridLinesVisible(true);
+//		grid.setGridLinesVisible(true);
 
 		tfDigit1 = new TextField("1");
 		tfDigit1.setPrefColumnCount(1);
-		grid.add(tfDigit1, 0, 1);
 		btUp1 = new Button();
-		grid.add(btUp1, 0, 0);
+		btUp1.setId("11");
 		btDown1 = new Button();
-		grid.add(btDown1, 0, 2);
+		btDown1.setId("12");
 		btUp1.setOnAction(eh);
+		btDown1.setOnAction(eh);
+
+		VBox vb1 = new VBox();
+		vb1.setAlignment(Pos.CENTER);
+		vb1.getChildren().add(btUp1);
+		vb1.getChildren().add(tfDigit1);
+		vb1.getChildren().add(btDown1);
+		grid.add(vb1, 0, 0);
 
 		tfDigit2 = new TextField("1");
 		tfDigit2.setPrefColumnCount(1);
-		grid.add(tfDigit2, 2, 1);
 		btUp2 = new Button();
-		grid.add(btUp2, 2, 0);
+		btUp2.setId("21");
 		btDown2 = new Button();
-		grid.add(btDown2, 2, 2);
+		btDown2.setId("22");
+		btUp2.setAlignment(Pos.CENTER);
 		btUp2.setOnAction(eh);
+		btDown2.setOnAction(eh);
+
+		VBox vb2 = new VBox();
+		vb2.setAlignment(Pos.CENTER);
+		vb2.getChildren().add(btUp2);
+		vb2.getChildren().add(tfDigit2);
+		vb2.getChildren().add(btDown2);
+		grid.add(vb2, 1, 0);
 
 		tfDigit3 = new TextField("1");
 		tfDigit3.setPrefColumnCount(1);
-		grid.add(tfDigit3, 4, 1);
 		btUp3 = new Button();
-		grid.add(btUp3, 4, 0);
+		btUp3.setId("31");
 		btDown3 = new Button();
-		grid.add(btDown3, 4, 2);
+		btDown3.setId("32");
 		btUp3.setOnAction(eh);
+		btDown3.setOnAction(eh);
+
+		VBox vb3 = new VBox();
+		vb3.setAlignment(Pos.CENTER);
+		vb3.getChildren().add(btUp3);
+		vb3.getChildren().add(tfDigit3);
+		vb3.getChildren().add(btDown3);
+		grid.add(vb3, 2, 0);
 
 		tfDigit4 = new TextField("1");
 		tfDigit4.setPrefColumnCount(1);
-		grid.add(tfDigit4, 6, 1);
 		btUp4 = new Button();
-		grid.add(btUp4, 6, 0);
+		btUp4.setId("41");
 		btDown4 = new Button();
-		grid.add(btDown4, 6, 2);
+		btDown4.setId("42");
 		btUp4.setOnAction(eh);
+		btDown4.setOnAction(eh);
+
+		VBox vb4 = new VBox();
+		vb4.setAlignment(Pos.CENTER);
+		vb4.getChildren().add(btUp4);
+		vb4.getChildren().add(tfDigit4);
+		vb4.getChildren().add(btDown4);
+		grid.add(vb4, 3, 0);
 
 
 		Scene scene = new Scene(grid, 300, 275);
