058ff148f2d51aea97411073c19bcb45d397fe0a
RomeoGolf <romeogolf@mail.ru>
2014-01-24 11:11:26 +0600

    Введен класс хранения данных попытки - четверка, число быков, число коров.

---
+++ src/romeogolf/ShotData.java
@@ -0,0 +1,28 @@
+package romeogolf;
+
+public class ShotData {
+	private Integer bulls;
+	private Integer cows;
+	private Integer[] qwad = new Integer[4];
+
+	public Integer getBulls(){
+		return bulls;
+	}
+
+	public Integer getCows(){
+		return cows;
+	}
+
+	public Integer[] getQwad(){
+		return qwad;
+	}
+
+	public ShotData(Integer bulls, Integer cows, Integer[] qwad) {
+		this.bulls = bulls;
+		this.cows = cows;
+		for(int i = 0; i < 4; i++){
+			this.qwad[i] = qwad[i];
+		}
+	}
+
+}
