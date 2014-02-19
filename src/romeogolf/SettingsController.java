16e52b1df84cddf7fdfcf5824ecbf680b1bf5427
RomeoGolf <romeogolf@mail.ru>
2014-02-19 16:20:54 +0600

    Добавлена fxml-заготовка формы настроек и заготовка ее контроллера.

---
+++ src/romeogolf/SettingsController.java
@@ -0,0 +1,20 @@
+package romeogolf;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class SettingsController implements Initializable{
+
+	public SettingsController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
