43d0914222ba6c96a9cf86c6047bbd78872e0fb3
RomeoGolf <romeogolf@mail.ru>
2014-02-20 11:33:53 +0600

    В контроллере окна настроек вписаны элементы управления и заготовка обработчиков событий.

--- src/romeogolf/SettingsController.java
+++ src/romeogolf/SettingsController.java
@@ -1,20 +1,49 @@
 package romeogolf;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
+import javafx.stage.Stage;
+
+
 
 public class SettingsController implements Initializable{
+	// переключатели права первого хода
+	@FXML private RadioButton rbFirstStep0;
+	@FXML private RadioButton rbFirstStep1;
+	@FXML private RadioButton rbFirstStep2;
+	@FXML private RadioButton rbFirstStep3;
+	//флажок сброса цифр в исходное (1234)
+	@FXML private CheckBox cbDigitsReset;
 
-	public SettingsController() {
-		// TODO Auto-generated constructor stub
+	private ArrayList<RadioButton> aFirstStep = new ArrayList<RadioButton>();
+
+	@FXML protected void onOK(ActionEvent e){
+		stage.close();
 	}
 
+	@FXML protected void onCancel(ActionEvent e){
+		stage.close();
+	}
+
+	Stage stage;
+    // получение ссылки на окно, установка обработчика событий окна 
+    public void setStage_Listener(final Stage stage){
+    	this.stage = stage;
+    }
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		
+		aFirstStep.add(rbFirstStep0);
+		aFirstStep.add(rbFirstStep1);
+		aFirstStep.add(rbFirstStep2);
+		aFirstStep.add(rbFirstStep3);
 	}
 
 }
