608a4140bc60e8884f77be8cccd0897b20f65ad8
RomeoGolf <romeogolf@mail.ru>
2019-04-17 11:26:27 +0500

    Сделал self-contained приложение, исправил напоминания
    Сделал файл jar средствами IDE (добавился манифест).
    Добавил файлы с командами создания jar, jmod и образов jre из
    out-директории с классами, файла jar и файла jmod.

---
+++ make-jmod.cmd
@@ -0,0 +1,3 @@
+c:\java\jdk-11.0.2\bin\jmod.exe create --class-path out\artifacts\romeogolf_bullscows_jar\romeogolf.bullscows.jar --main-class=romeogolf.bc.Main out-jmod\romeogolf.bullscows.jmod
+
+@pause
